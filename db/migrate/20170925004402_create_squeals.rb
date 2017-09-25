57c9b45123c37f7974bd73970adbe95adf9ad47e
DeadFred <f.cliff1@hotmail.com>
2017-09-25 10:46:48 +1000

    Created Squeal scaffolding

---
+++ db/migrate/20170925004402_create_squeals.rb
@@ -0,0 +1,10 @@
+class CreateSqueals < ActiveRecord::Migration[5.1]
+  def change
+    create_table :squeals do |t|
+      t.string :content
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/system/squeals_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class SquealsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit squeals_url
+  #
+  #   assert_selector "h1", text: "Squeal"
+  # end
+end
